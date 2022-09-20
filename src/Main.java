90e3d10d030d9b53c71ab5393108bb8672d125f7
Алекс <chu@mail.ru>
2022-09-20 17:12:40 +0300

    cat

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ var cat = 3.6;
 var papper = 763789;
 System.out.println(cat);
 System.out.println(papper);
+dog = dog + 4;
+cat = cat +4;
+papper = papper + 4;
+System.out.println(dog);
+System.out.println(cat);
+System.out.println(papper);
     }
 }
