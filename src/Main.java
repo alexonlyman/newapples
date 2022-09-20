d6333f8e448a27d6761bc349a13c9cbdebda8e3f
Алекс <chu@mail.ru>
2022-09-20 17:07:26 +0300

    cat

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+var dog = 8;
+System.out.println(dog);
+var cat = 3.6;
+var papper = 763789;
+System.out.println(cat);
+System.out.println(papper);
+    }
+}
